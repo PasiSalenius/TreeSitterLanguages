16b6a58c60dc5ad4482e62597851ed3cc8850f8f
Pasi Salenius <pasi@freshbits.fi>
2024-10-06 18:51:33 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "TreeSitterLanguages",
-    platforms: [.iOS(.v14)],
+    platforms: [.iOS(.v14), .macOS(.v11)],
     products: [
         .library(name: "TreeSitterLanguagesCommon", targets: ["TreeSitterLanguagesCommon"]),
         .library(name: "TreeSitterAstro", targets: ["TreeSitterAstro"]),
